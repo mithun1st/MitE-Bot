b07ce71e9ae53788cb83c6438ae82ec9a1efd9cf
Mahadi Hassan <mithun.2121@yahoo.com>
2020-10-22 18:36:09 +0600

    ui1

---
+++ src/mit/e/bot/MitEBot.java
@@ -0,0 +1,11 @@
+
+package mit.e.bot;
+
+
+public class MitEBot {
+
+    public static void main(String[] args) {
+        new frm().setVisible(true);
+    }
+
+}
---
+++ src/mit/e/bot/cls.java
@@ -0,0 +1,11 @@
+
+package mit.e.bot;
+
+
+public class cls {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
